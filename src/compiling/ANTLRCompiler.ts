e6ab887e0ca6761c692fd98bc0ddd97b30292d6b
mwelias <mwelias@ncsu.edu>
2019-10-22 11:32:33 -0400

    Added simple identifier checking for nodes

--- src/compiling/ANTLRCompiler.ts
+++ src/compiling/ANTLRCompiler.ts
@@ -6,13 +6,14 @@ import {
     TextDocument
 } from "vscode-languageserver";
 import { DocumentCompiler, DiagnosticData, isDiagnosticDataUndefined } from './DocumentCompiler';
-import { ANTLRInputStream, CommonTokenStream, ConsoleErrorListener, Token, CharStream, InputMismatchException} from 'antlr4ts';
+import { ANTLRInputStream, CommonTokenStream, ConsoleErrorListener, Token, CharStream, InputMismatchException, RuleContext} from 'antlr4ts';
 import {SystemVerilogLexer} from './ANTLR/grammar/build/SystemVerilogLexer'
 import {SystemVerilogParser, System_verilog_textContext} from './ANTLR/grammar/build/SystemVerilogParser'
 import {SyntaxErrorListener} from './ANTLR/SyntaxErrorListener'
 import { isSystemVerilogDocument, isVerilogDocument } from '../utils/server';
 import { ParseTree } from "antlr4ts/tree/ParseTree";
 import { Tree } from "antlr4ts/tree/Tree";
+import { AbstractTokenTree } from "./ANTLR/AbstractTokenTree";
 
 export class ANTLRCompiler extends DocumentCompiler {
 
@@ -70,12 +71,19 @@ export class ANTLRCompiler extends DocumentCompiler {
     }
 
     private generateVariablesAbstractTree(concreteTree : System_verilog_textContext) {
-        let absIdentifierTree : Tree;
+        let tokenTreeRoot : AbstractTokenTree;
         let concreteChildren : ParseTree[] = concreteTree.children;
+        let index = 0;
+        let ruleNames : string[] = this.simpleIdentifiers();
+        let identfierToken : Token = null;
         concreteChildren.forEach((child: ParseTree) => {
-            // if(isSimpleIdentifier(child.payload)){
-            //     //TODO construct the tree
-            // }
+            if(child.payload instanceof RuleContext){
+                if(this.isSimpleIdentifier(child.payload.toString(ruleNames))){
+                    let identifierToken = this.findSimpleIdentifierLeaf(index, concreteTree);
+                    index += child.childCount;
+                }
+            }
+            index++;
         })
     }
 
@@ -91,6 +99,24 @@ export class ANTLRCompiler extends DocumentCompiler {
         let tempLexer : SystemVerilogLexer = new SystemVerilogLexer(chStream);
         let tokenType = tempLexer.nextToken().type;
         return concreteTree.tryGetToken(tokenType, leafIndex)._symbol;
+    }
+
+    private simpleIdentifiers() : string[] {
+        return new Array(
+            "array_identifier", "block_identifier", "bin_identifier", "cell_identifier", "checker_identifier", "class_identfier",
+            "clocking_identifier", "config_identifier", "const_identifier", "constraint_identifier", "covergroup_identifier", "cover_point_identifier",
+            "cross_identifier", "enum_identifier", "formal_identifier", "formal_port_identifier", "function_identifier", "generate_block_identifier",
+            "genvar_identifier", "index_variable_identifier", "interface_identifier", "interface_instance_identifier", "inout_port_identifier",
+            "input_port_identifier", "instance_identifier", "library_identifier", "memeber_identifier", "method_identifier", "modport_identifier",
+            "module_identifier", "net_identifier", "net_type_identifier", "output_port_identifier", "package_identifier", "parameter_identifier",
+            "port_identifier", "production_identifier", "program_identifier", "property_identifier", "sequence_identifier", "signal_identifier", 
+            "specparam_identifier", "task_identifier", "tf_identifier", "terminal_identifier", "topmodule_identifier", "type_identifier", "udp_identifier",
+            "variable_identifier"
+        );
+    }
+
+    private isSimpleIdentifier(ruleName : string) : boolean {
+        return ruleName !== "" && ruleName !== null;
     }
 
     /**
